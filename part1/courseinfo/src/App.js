ed1ae7c3737fd1acc61c775ee74de7245aeb9f5a
hornpondbrook <hornpondbrook@gmail.com>
2023-05-08 15:29:08 -0400

    part1 courseinfo exercises 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -13,38 +15,157 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {props.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)}
     </div>
   )
 }
 
 const Total = (props) => {
+  let exercisesNumber = 0
+  props.parts.forEach(element => {
+    exercisesNumber += element.exercises
+  });
   return (
-    <p>Number of exercises {props.exercisesNumber}</p>
+    <p>Number of exercises {exercisesNumber}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  //const course = 'Half Stack application development'
+
+  /*
+  const parts = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  */
+
+  /*
+  const parts = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  } 
+  */ 
+
+  /*
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  */
+
+  /*
+  const Display = (props) => (
+    <div>{props.counter}</div>
+  )
+
+  const CounterButton = ({ onEvent, text }) => (
+    <button onClick={onEvent}>{text}</button>
+  )
+
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    setCounter(0)
+  }
+
+  console.log(`rendering... ${counter}`)
+  return (
+    <div>
+      <Display counter={counter}></Display>
+      <CounterButton onEvent={increaseCounter} text="plus"></CounterButton>
+      <CounterButton onEvent={decreaseCounter} text="minus"></CounterButton>
+      <CounterButton onEvent={setToZero} text="zero"></CounterButton>
+    </div>
+  )
+  */ 
+
+  /*
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+    setTotal(total + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+    setTotal(total + 1)
+  }
+
+  debugger 
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        part1={part1} part2={part2} part3={part3} 
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} 
-      />
-      <Total exercisesNumber={exercises1 + exercises2 + exercises3} />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
+  */
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
 }
 
 export default App;
